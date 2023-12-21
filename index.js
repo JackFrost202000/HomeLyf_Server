feae3589e374b7f5a53651ee42bb9290e17d1fc6
JackFrost202000 <152257557+JackFrost202000@users.noreply.github.com>
2023-12-21 13:17:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose")
 const cors = require("cors");
 
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 //Import from other files
 const authRouter = require('./routes/auth');
@@ -12,7 +14,7 @@ const productRouter = require("./routes/product");
 //INIT
 const PORT = process.env.PORT || 3000;
 const app = express();
-const DB = "mongodb+srv://amitpotdukhe20:cmPUrefrPzB0BIuW@homelyfcluster0.4fzxngg.mongodb.net/?retryWrites=true&w=majority";
+const DB = "mongodb+srv://jackfrost2001:EhOLjUkP877mRXHs@cluster0.wnjkzcd.mongodb.net/?retryWrites=true&w=majority";//"mongodb+srv://amitpotdukhe20:cmPUrefrPzB0BIuW@homelyfcluster0.4fzxngg.mongodb.net/?retryWrites=true&w=majority";
 
 //middleware
 app.use(cors());
@@ -28,6 +30,25 @@ mongoose.connect(DB).then(()=>{
 .catch((e)=>{
     console.log(e);
 });
+
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'HomeLyf API Testing',
+        version: '1.0.0',
+      },
+      servers:[
+        {
+            url: 'https://homelyf-server.onrender.com'
+        }
+      ]
+    },
+    apis: ['./routes/*.js'],
+  };
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.listen(PORT, "0.0.0.0", ()=>{
     console.log(`connected at port ${PORT}`);
